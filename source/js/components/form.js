fa87484cbb1ef48651b64ba2267151ce1c7599ed
Guilherme Santiago <guilherme.santiago95@gmail.com>
2017-04-10 11:12:56 -0300

    Improve form validation

--- source/js/components/form.js
+++ source/js/components/form.js
@@ -9,88 +9,112 @@ import 'jquery-validation'
 import '../localization/validation'
 
 /**
+ * @class Form
+ */
+
+class Form {
+  constructor (form, options = {}) {
+    const dataOptions = extractDataOptions(form, 'form')
+
+    this.$form = $(form)
+    this.options = $.extend({}, Form.defaults, options, dataOptions)
+    this.pending = false
+    this.$submitButton = this.$form.find('[type="submit"]')
+    this.submitButtonHtml = this.$submitButton.html()
+
+    this.addValidations()
+  }
+
+  addValidations () {
+    this.$form.validate({
+      errorElement: 'span',
+
+      errorPlacement ($error, $input) {
+        $error.addClass('ui-error')
+        $input.parents('.ui-control').append($error)
+      },
+
+      submitHandler: this.handleSubmit.bind(this)
+    })
+  }
+
+  handleSubmit (form) {
+    if (this.pending) {
+      return
+    }
+
+    if (this.options.disableButtonOnSubmit) {
+      this.disableSubmitButton()
+    }
+
+    this.pending = true
+
+    if (!this.options.ajax) {
+      return form.submit()
+    }
+
+    this.performAjaxRequest()
+  }
+
+  disableSubmitButton () {
+    this.$submitButton
+      .attr('disabled', true)
+      .html(this.options.disabledButtonHtml)
+  }
+
+  enableSubmitButton () {
+    this.$submitButton
+      .removeAttr('disabled')
+      .html(this.submitButtonHtml)
+  }
+
+  performAjaxRequest () {
+    const { feedbackHandler } = this.options
+    const url = this.$form.attr('action')
+    const method = this.$form.attr('method')
+    const data = this.$form.serializeArray()
+
+    $.ajax({
+      url,
+      method,
+      data,
+      dataType: 'json'
+    })
+    .done(feedbackHandler)
+    .fail(err => {
+      feedbackHandler({message: 'Erro ao enviar dados!'})
+    })
+    .always(() => {
+      this.pending = false
+
+      if (this.options.disableButtonOnSubmit) {
+        this.enableSubmitButton()
+      }
+    })
+  }
+}
+
+Form.defaults = {
+  ajax: false,
+  disableButtonOnSubmit: true,
+  disabledButtonHtml: 'Processando...',
+  feedbackHandler (feedback) {
+    const type = feedback.success
+    ? 'success'
+    : 'error'
+
+    const title = feedback.title || ''
+    const text = feedback.message
+
+    customAlert({type, title, text})
+  }
+}
+
+/**
  * Get all `.js-form` elements.
  */
 
 $('.js-form').each(function () {
-  createForm($(this))
+  const form = new Form(this)
+  $(this).data('form', form)
 })
-
-/**
- * Create the form with validations.
- */
-
-export default function createForm ($form, options = {}) {
-  const defaults = {
-    ajax: false,
-    disableButtonOnSubmit: true,
-    disabledButtonHTML: 'Processando...'
-  }
-
-  const dataOptions = extractDataOptions($form[0], 'form')
-  const settings = $.extend({}, defaults, options, dataOptions)
-  let pending = false
-
-  const url = $form.attr('action')
-  const method = $form.attr('method')
-  const $button = $form.find('[type="submit"]')
-  const buttonHTML = $button.html()
-
-  $form.validate({
-    errorElement: 'span',
-
-    errorPlacement ($error, $input) {
-      $error.addClass('ui-error')
-      $input.parents('.ui-control').append($error)
-    },
-
-    submitHandler (form) {
-      if (pending) {
-        return
-      }
-
-      pending = true
-
-      if (settings.disableButtonOnSubmit) {
-        $button.attr('disabled', true).html(settings.disabledButtonHTML)
-      }
-
-      if (!settings.ajax) {
-        return form.submit()
-      }
-
-      const data = $form.serializeArray()
-
-      $.ajax({url, method, data, dataType: 'json'})
-        .done(response => {
-          feedbackHandler(response)
-        })
-        .fail(err => {
-          feedbackHandler({message: 'Erro ao processar dados!'})
-          console.log('ajax error:', err)
-        })
-        .always(() => {
-          if (settings.disableButtonOnSubmit) {
-            $button.html(buttonHTML).removeAttr('disabled')
-          }
-
-          pending = false
-        })
-    }
-  })
-}
-
-/**
- * feedbackHandler
- */
-
-function feedbackHandler (feedback) {
-  const type = feedback.success
-    ? 'success'
-    : 'error'
-
-  const title = feedback.title || ''
-  const text = feedback.message
-
-  customAlert({type, title, text})
-}
